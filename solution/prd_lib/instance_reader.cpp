d99dca4d84c4009431e2bbfd654a1c158f7ebe7f
Hilleriteau Jean <jhi@live.fr>
2019-01-24 16:18:29 +0100

    netoyage du code de lecture

--- solution/prd_lib/instance_reader.cpp
+++ solution/prd_lib/instance_reader.cpp
@@ -11,54 +11,30 @@ namespace {
 	{
 		return complet_string.size() >= start_string.size() && std::equal(start_string.begin(), start_string.end(), complet_string.begin());
 	}
-}
-
-/*
-	read an instance from an input stream
-*/
-instance instance_reader::read(std::istream & input, index_type job_per_batch)
-{
-	// some datas are present in the readed file format but not used in this program
-
-	index_type job_count;
-	index_type machine_count;
-	index_type vehicule_cost; // unused
-	index_type batch_count;
-
-	input >> job_count >> machine_count >> vehicule_cost;
-
-	//batch_count = job_count / job_per_batch;
-	index_type partial_batch = (job_count % job_per_batch) ? 1 : 0;
-	batch_count = (job_count / job_per_batch + partial_batch);
-
-	input.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // end of the second line
-
-	time_unit first_possible_departure{ 0 };
-	std::vector<batch> batchs(batch_count);
-	std::generate(batchs.begin(), --batchs.end(), [&]() {
-		std::vector<job> jobs(job_per_batch);
-		std::generate(jobs.begin(), jobs.end(), [&]() {
-			job result = read_job(input, machine_count);
-			go_to_next_line(input);
-			return result;
-		});
-		return batch(jobs);
-	});
+	std::vector<batch> read_batchs(std::istream & input, index_type batch_count, index_type job_per_batch, index_type machine_count, index_type job_count)
 	{
-		std::vector<job> jobs(job_count - (batch_count - 1) * job_per_batch);
-		std::generate(jobs.begin(), jobs.end(), [&]() {
-			job result = read_job(input, machine_count);
-			go_to_next_line(input);
-			return result;
+		std::vector<batch> batchs(batch_count);
+		std::generate(batchs.begin(), --batchs.end(), [&]() {
+			std::vector<job> jobs(job_per_batch);
+			std::generate(jobs.begin(), jobs.end(), [&]() {
+				job result = read_job(input, machine_count);
+				go_to_next_line(input);
+				return result;
+			});
+			return batch(jobs);
 		});
-		batchs.back() = batch( jobs );
+		{
+			std::vector<job> jobs(job_count - (batch_count - 1) * job_per_batch);
+			std::generate(jobs.begin(), jobs.end(), [&]() {
+				job result = read_job(input, machine_count);
+				go_to_next_line(input);
+				return result;
+			});
+			batchs.back() = batch(jobs);
+		}
+		return batchs;
 	}
-
-	instance result;
-	result.set_batchs(batchs);
-	result.set_job_number(job_count);
-	result.set_machine_number(machine_count);
-
+	void read_distancier_title(std::istream & input)
 	{
 		std::string row;
 		std::getline(input, row);
@@ -67,6 +43,34 @@ instance instance_reader::read(std::istream & input, index_type job_per_batch)
 			throw std::exception("invalid input format, 'Distancier :' expected");
 		}
 	}
+}
+
+/*
+	read an instance from an input stream
+*/
+instance instance_reader::read(std::istream & input, index_type job_per_batch)
+{
+	// some datas are present in the readed file format but not used in this program
+	instance result;
+
+	index_type job_count;
+	index_type machine_count;
+	index_type vehicule_cost; // unused
+	index_type batch_count;
+	input >> job_count >> machine_count >> vehicule_cost;
+	
+	result.set_job_number(job_count);
+	result.set_machine_number(machine_count);
+
+	index_type partial_batch = (job_count % job_per_batch) ? 1 : 0;
+	batch_count = (job_count / job_per_batch + partial_batch);
+
+	go_to_next_line(input); // end of the second line
+
+	auto batchs = read_batchs(input, batch_count, job_per_batch, machine_count, job_count);
+	result.set_batchs(batchs);
+
+	read_distancier_title(input);
 
 	double deposit_coord_x, deposit_coord_y; // not used
 	input >> deposit_coord_x >> deposit_coord_y;
